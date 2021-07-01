b9a46f6f2c83a4afae54ca4b9d60ec8d3b073b76
nathan-rdrgz <nathan.rdrgz@googlemail.com>
2021-06-30 18:12:39 -0700

    FIX plot attributes

--- VWAP.R
+++ VWAP.R
@@ -1,7 +1,7 @@
 
 library(ggplot2)
-library(data.table)
 library(quantmod)
+library(data.table)
 
 source('functions.R')
 
@@ -9,9 +9,13 @@ DT <- GetStockData(symbol = 'VZIO')
 moltenDT <- melt.data.table(DT, id.vars = c('Date'))
 
 ggplot(moltenDT[variable %in% c('CLOSE','OPEN', 'vwap')],
-       aes(x = Date, y = value, color = variable)) + 
+       aes(x = as.Date(Date), y = value, color = variable)) + 
   geom_line(size = 1.5) +
   xlab('') +
+  scale_x_date(breaks = scales::date_breaks('1 week'), 
+               labels = scales::date_format(format = '%b %d')) +
   theme_classic() +
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1 ))
+
 
